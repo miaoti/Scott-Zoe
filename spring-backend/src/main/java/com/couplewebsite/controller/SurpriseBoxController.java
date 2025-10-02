950221d497a95a03c782d686e5d6c68bfa479b42
miao tingshuo <tingshuo_miao2@baylor.edu>
2025-10-02 06:43:14 -0500

    a

--- spring-backend/src/main/java/com/couplewebsite/controller/SurpriseBoxController.java
+++ spring-backend/src/main/java/com/couplewebsite/controller/SurpriseBoxController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
